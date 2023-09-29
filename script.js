fe2044eb2e3c757eb140bc02edea4cb61d1c712c
aaminur1134 <arkaminur932@gmail.com>
2023-09-29 21:11:04 +0600

    added a copy button and change the selector

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
-const hexColorBtn = document.getElementById('hexBtn');
+const hexColorBtn = document.querySelector('#hexBtn');
 const bgColor = document.querySelector('body');
-const hexCode = document.getElementById('hexColor');
 
-hexColorBtn.addEventListener('click' , function(){
+const hexCode = document.querySelector('#hexColor');
+const copyBtn = document.querySelector('#copy-btn');
+
+
+hexColorBtn.addEventListener('click', function () {
     hexColor = '#';
     for (let i = 0; i < 6; i++) {
-        const randomColor = Math.floor(Math.random()*hexNumbers.length);
+        const randomColor = Math.floor(Math.random() * hexNumbers.length);
         hexColor += hexNumbers[randomColor];
     }
+
     bgColor.style.backgroundColor = hexColor;
     hexCode.innerHTML = hexColor;
+    hexCode.value = hexColor;
+});
+
+copyBtn.addEventListener('click', function () {
+    navigator.clipboard.writeText(hexCode.value);
 });
 
